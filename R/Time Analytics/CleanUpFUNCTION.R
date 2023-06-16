cb1cb0e076b3cf261401f32e1f05469b4c45c49e
Ayush548 <ayushgarg0728@gmail.com>
2023-06-16 12:39:38 -0400

    comments

--- R/Time Analytics/CleanUpFUNCTION.R
+++ R/Time Analytics/CleanUpFUNCTION.R
@@ -1,4 +1,4 @@
-# Function to combine two columns and remove the original columns
+# Function to sum two columns and remove the original columns
 combiner <- function(data, colnew, col1, col2){
   data[colnew] <- data[col1] + data[col2]
   data <- data[, !(colnames(data) %in% c(col1, col2))]
@@ -6,8 +6,7 @@ combiner <- function(data, colnew, col1, col2){
     data
 }
 
-# Function to Clean up
-#inputs and outputs data
+# Function to Clean up data
 CleanUp <- function(data) {
 
   #combine data
@@ -17,7 +16,7 @@ CleanUp <- function(data) {
   data <- combiner(data, "Family", "Family.Time..walking.etc.", "Golf.Dad.Sports")
   data <- combiner(data, "projects", "Technical.Projects", "Other.Projects")
 
-  #Make sum 1440
+  #Make sum 1440 for uniformness across days
   for(i in 1:(nrow(data))) {   
     sum <- rowSums(data[i,])
     data[i,] <- data[i,]/sum

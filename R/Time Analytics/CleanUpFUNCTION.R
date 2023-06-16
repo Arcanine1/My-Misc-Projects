82b46ec0fccacd65e8df99713f7c897a72806e0a
Ayush548 <ayushgarg0728@gmail.com>
2023-06-16 10:36:21 -0400

    Makes uniform sum for rows in Cleanup function

--- R/Time Analytics/CleanUpFUNCTION.R
+++ R/Time Analytics/CleanUpFUNCTION.R
@@ -10,15 +10,20 @@ combiner <- function(data, colnew, col1, col2){
 #inputs and outputs data
 CleanUp <- function(data) {
 
+  #combine data
   data <- combiner(data, "Internships", "Internships..other.", "Palav")
-
   data <- combiner(data, "friends", "RW", "hanging.out.with.friends")
-
   data <- combiner(data, "OnlineSocial", "FaceTime", "Texting")
-
   data <- combiner(data, "Family", "Family.Time..walking.etc.", "Golf.Dad.Sports")
-
   data <- combiner(data, "projects", "Technical.Projects", "Other.Projects")
 
+  #Make sum 1440
+  for(i in 1:(nrow(data))) {   
+    sum <- rowSums(data[i,])
+    data[i,] <- data[i,]/sum
+    data[i,] <- data[i,] * 1440
+  }
    data
 }
+
+

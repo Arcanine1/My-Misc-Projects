551ccf6e11911b988f0e754c1938d1c8e7d6fdc8
Ayush548 <ayushgarg0728@gmail.com>
2023-07-11 03:13:56 -0400

    Code not working but V1 started

--- Voice Modulator/Functions.py
+++ Voice Modulator/Functions.py
@@ -1,6 +1,23 @@
 import scipy.io.wavfile as wav
-import Functions
+from playsound import playsound
+import random
 
+#imports data and normalizes data to -1 to 1
+#outputs normalized signal and data rate
 def getData(fileName):
     fs, signal = wav.read(fileName)
-    signal = signal / 32767 # 2**15 - 1
+    return signal,fs
+
+#plays sound and saves file
+def play(sound,fs):
+    wav.write('Voice Modulator/Scrambled.wav', fs, sound)
+    playsound('Voice Modulator/Scrambled.wav')
+
+#scrambles sound
+def scramble(sound):
+    max = sound.max()
+    sound = sound/max
+
+    for i,row in enumerate(sound):
+        for j,entry in enumerate(row):
+            sound[i][j] = entry + random.gauss(1,.1)
--- Voice Modulator/Scrambler.py
+++ Voice Modulator/Scrambler.py
@@ -1,2 +1,7 @@
-import Functions
+from Functions import *
+
+fileName = "Voice Modulator/Spanish.wav"
+sound, fs = getData(fileName)
+scramble(sound)
+play(sound,fs)
 

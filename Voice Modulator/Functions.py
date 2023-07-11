0efe147aabf459baba432298aedde11408690b28
Ayush548 <ayushgarg0728@gmail.com>
2023-07-11 18:05:07 -0400

    Added plot function for trouble shooting

--- Voice Modulator/Functions.py
+++ Voice Modulator/Functions.py
@@ -1,5 +1,7 @@
 import scipy.io.wavfile as wav
 from playsound import playsound
+import matplotlib.pyplot as plt
+import numpy
 import random
 
 #imports data and normalizes data to -1 to 1
@@ -21,3 +23,14 @@ def scramble(sound):
     for i,row in enumerate(sound):
         for j,entry in enumerate(row):
             sound[i][j] = entry + random.gauss(1,.1)
+
+#plots the sound
+def plot(data):
+    import numpy as np
+    time = np.linspace(0., 1, data.shape[0])
+    plt.plot(time, data[:, 0], label="Left channel")
+    plt.plot(time, data[:, 1], label="Right channel")
+    plt.legend()
+    plt.xlabel("Time [s]")
+    plt.ylabel("Amplitude")
+    plt.show()
--- Voice Modulator/Scrambler.py
+++ Voice Modulator/Scrambler.py
@@ -2,6 +2,7 @@ from Functions import *
 
 fileName = "Voice Modulator/Spanish.wav"
 sound, fs = getData(fileName)
+plot(sound)
 scramble(sound)
 play(sound,fs)
 

739942789788dafa381e1b7e10b4be8945932e56
Arcanine1 <ayushgarg0728@gmail.com>
2023-07-17 01:44:05 -0400

    Back to Functional Programming

--- Voice Modulator/Scrambler.py
+++ Voice Modulator/Scrambler.py
@@ -1,9 +1,8 @@
 from Functions import *
 
 fileName = "Voice Modulator/Spanish.wav"
-sound = Sound(fileName= fileName)
-sound.sound= sound.sound*5
+sound, fs = importSound(fileName= fileName)
+sound= sound*5
 
-sound.plotFourierTransform()
-
-sound.play()
+plotFourierTransform(sound)
+play(sound,fs)

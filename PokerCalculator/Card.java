d21c5605ec0619e723d9c5383ffc2d00f9dc39f1
Ayush548 <ayushgarg0728@gmail.com>
2023-07-29 23:33:23 -0400

    Finished Hand Class untested

--- PokerCalculator/Card.java
+++ PokerCalculator/Card.java
@@ -2,14 +2,14 @@ package PokerCalculator;
 
 public class Card{
 
-    // suit 1-4 for the 4 suits
+    // suit 0-3 for the 4 suits
     // cards 1-13 for the cards
-    public int mySuit;
-    public int myCard;
+    public int suit;
+    public int card;
 
     public Card(int suit, int card){
-        mySuit = suit;
-        myCard = card;
+        this.suit = suit;
+        this.card = card;
     }
 
 }
--- PokerCalculator/Deck.java
+++ PokerCalculator/Deck.java
@@ -5,38 +5,22 @@ import java.util.Random;
 
 public class Deck {
 
-    public ArrayList<Card> Deck;
+    public ArrayList<Card> deck;
     
     //creates deck of size deck size
-    public Deck(int deckSize) throws IllegalAccessException{
-
-        if(deckSize <1  ){
-            throw new IllegalAccessException("deckSize must be more than 0");
-        }
-
-        for( int i = 0; i<deckSize; i++){
-            addNewDeck();
-        }
+    public Deck(){
+        for (int suit =0; suit<4; suit++){
+                    for (int number =1; number<14; number++){
+                        Card card =  new Card (suit,number);
+                        deck.add(card);
+            }
     }
+}
 
     //Gets Random Card and removes the card from the deck
     public Card randomCard(){
         Random rand = new Random();
-        int index = rand.nextInt(Deck.size());
-        return Deck.remove(index);
+        int index = rand.nextInt(deck.size());
+        return deck.remove(index);
     }
-
-    //add new deck to ArrayList
-    private void addNewDeck(){
-
-        for (int suit =1; suit<5; suit++){
-                    for (int number =1; number<14; number++){
-                        Card card =  new Card (suit,number);
-                        Deck.add(card);
-            }
-        }
-    }
-
-
-
 }
